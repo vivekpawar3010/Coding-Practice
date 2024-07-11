e3e8207c6064fa517a01ed083c26f7130639c13c
Vivek Pawar <160049777+vivekpawar3010@users.noreply.github.com>
2024-07-11 19:48:24 +0530

    the code of string study
    1190. Reverse Substrings Between Each Pair of Parentheses

---
+++ leetcode11.cpp
@@ -0,0 +1,26 @@
+
+class Solution {
+public:
+    string reverseParentheses(string s) {
+        stack<int> st;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] =='(') {
+                st.push(i);
+            } else if (s[i]== ')') {
+                int j = st.top();
+                st.pop();
+                reverse(s.begin() + j + 1, s.begin() + i);
+            }
+        }
+
+        string result;
+        for (char c : s) {
+            if (c != '(' && c != ')') {
+                result += c;
+            }
+        }
+
+        return result;
+    }
+};
