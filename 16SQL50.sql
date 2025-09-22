4fadfdc43b8583ea91a8fd5575486d0443cac524
vivekpawar3010 <vivekpawar932564@gmail.com>
2025-09-22 19:49:53 +0530

    Added 16SQL50.sql

---
+++ 16SQL50.sql
@@ -0,0 +1,16 @@
+--+-------------+------+
+--| Column Name | Type |
+--+-------------+------+
+--| teacher_id  | int  |
+--| subject_id  | int  |
+--| dept_id     | int  |
+--+-------------+------+
+--(subject_id, dept_id) is the primary key (combinations of columns with unique values) of this table.
+--Each row in this table indicates that the teacher with teacher_id teaches the subject subject_id in the department dept_id.
+--Write a solution to calculate the number of unique subjects each teacher teaches in the university.
+--Return the result table in any order.
+--The result format is shown in the following example.
+
+--    ANS:- https://leetcode.com/problems/number-of-unique-subjects-taught-by-each-teacher/description/
+SELECT teacher_id,COUNT(DISTINCT subject_id ) AS cnt FROM Teacher 
+GROUP BY teacher_id
