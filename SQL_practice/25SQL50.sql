67f926629c8136b7a8bea4e0a56d70addd273866
vivekpawar3010 <vivekpawar932564@gmail.com>
2025-10-02 11:28:48 +0530

    update on the 2 oct 2025 a sql learning day 25

---
+++ SQL_practice/25SQL50.sql
@@ -0,0 +1,20 @@
+-- 1527. Patients With a Condition
+
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | patient_id   | int     |
+-- | patient_name | varchar |
+-- | conditions   | varchar |
+-- +--------------+---------+
+-- patient_id is the primary key (column with unique values) for this table.
+-- 'conditions' contains 0 or more code separated by spaces. 
+-- This table contains information of the patients in the hospital.
+-- Write a solution to find the patient_id, patient_name, and conditions of the patients who have Type I Diabetes. Type I Diabetes always starts with DIAB1 prefix.
+-- Return the result table in any order.
+-- The result format is in the following example.
+-- ANSWER: https://leetcode.com/problems/patients-with-a-condition/?envType=study-plan-v2&envId=top-sql-50
+select * from Patients
+where conditions like '% DIAB1%' or 
+      conditions like 'DIAB1%'
+order by patient_name;
