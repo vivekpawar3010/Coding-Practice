850984beb2bc84dbb1440e5ba2742b54ad76b29f
vivekpawar3010 <vivekpawar932564@gmail.com>
2025-10-25 07:27:39 +0530

    this is the date 25 oct maintaining the strike

---
+++ Cpp_Practice/25_CPP_150leetcode.cpp
@@ -0,0 +1,81 @@
+// 13. Roman to Integer
+
+// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+// Symbol       Value
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+// For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+// Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+// I can be placed before V (5) and X (10) to make 4 and 9. 
+// X can be placed before L (50) and C (100) to make 40 and 90. 
+// C can be placed before D (500) and M (1000) to make 400 and 900.
+// Given a roman numeral, convert it to an integer.
+
+ 
+
+// Example 1:
+
+// Input: s = "III"
+// Output: 3
+// Explanation: III = 3.
+// Example 2:
+
+// Input: s = "LVIII"
+// Output: 58
+// Explanation: L = 50, V= 5, III = 3.
+// Example 3:
+
+// Input: s = "MCMXCIV"
+// Output: 1994
+// Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 15
+// s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+// It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+
+
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> roman_values = {
+            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
+            {'C', 100}, {'D', 500}, {'M', 1000}
+        };
+        int total = 0;
+        int n = s.size();
+        for (int i = 0; i < n; ++i) {
+            if (i < n - 1 && roman_values[s[i]] < roman_values[s[i + 1]]) {
+                total -= roman_values[s[i]];
+            } else {
+                total += roman_values[s[i]];
+            }
+        }
+        return total;
+    }
+};
+
+int main() {
+    Solution sol;
+    string s1 = "III";
+    string s2 = "LVIII";
+    string s3 = "MCMXCIV";
+    int result1 = sol.romanToInt(s1); // Output: 3
+    int result2 = sol.romanToInt(s2); // Output: 58
+    int result3 = sol.romanToInt(s3); // Output: 1994
+    return 0;
+}
