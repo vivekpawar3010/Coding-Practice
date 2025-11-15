2d586d5979b66e271f972caa08f7d62e39e6392b
vivekpawar3010 <vivekpawar932564@gmail.com>
2025-11-15 07:25:46 +0530

    this is the date 15 nov update

---
+++ Cpp_Practice/15_NOV_150leetcode.cpp
@@ -0,0 +1,82 @@
+// 33. Search in Rotated Sorted Array
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// There is an integer array nums sorted in ascending order (with distinct values).
+
+// Prior to being passed to your function, nums is possibly left rotated at an unknown index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be left rotated by 3 indices and become [4,5,6,7,0,1,2].
+
+// Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+// Example 1:
+
+// Input: nums = [4,5,6,7,0,1,2], target = 0
+// Output: 4
+// Example 2:
+
+// Input: nums = [4,5,6,7,0,1,2], target = 3
+// Output: -1
+// Example 3:
+
+// Input: nums = [1], target = 0
+// Output: -1
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 5000
+// -104 <= nums[i] <= 104
+// All values of nums are unique.
+// nums is an ascending array that is possibly rotated.
+// -104 <= target <= 104
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int i = 0, j = nums.size() - 1;
+
+        while(i <= j) {
+            int mid = i + (j - i) / 2;
+
+            if(nums[mid] == target) return mid;
+
+            // Left half is sorted
+            if(nums[i] <= nums[mid]) {
+                if(nums[i] <= target && target < nums[mid]) {
+                    j = mid - 1;
+                } else {
+                    i = mid + 1;
+                }
+            }
+            // Right half is sorted
+            else {
+                if(nums[mid] < target && target <= nums[j]) {
+                    i = mid + 1;
+                } else {
+                    j = mid - 1;
+                }
+            }
+        }
+
+        return -1;  // target not found
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {4,5,6,7,0,1,2};
+    int target = 0;
+    int result = sol.search(nums, target);
+    cout << "Index of target " << target << " is: " << result << endl; // Output: 4
+    return 0;
+}
