cb0a4ffd97e1f16044974d4e662d8d497ee1ad2a
vivekpawar3010 <vivekpawar932564@gmail.com>
2025-11-23 18:05:18 +0530

    this is the update on date 23 nov

---
+++ Cpp_Practice/23_NOV_150leetcode.cpp
@@ -0,0 +1,63 @@
+
+// 120. Triangle
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Given a triangle array, return the minimum path sum from top to bottom.
+
+// For each step, you may move to an adjacent number of the row below. More formally, if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
+
+ 
+
+// Example 1:
+
+// Input: triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+// Output: 11
+// Explanation: The triangle looks like:
+//    2
+//   3 4
+//  6 5 7
+// 4 1 8 3
+// The minimum path sum from top to bottom is 2 + 3 + 5 + 1 = 11 (underlined above).
+// Example 2:
+
+// Input: triangle = [[-10]]
+// Output: -10
+ 
+
+// Constraints:
+
+// 1 <= triangle.length <= 200
+// triangle[0].length == 1
+// triangle[i].length == triangle[i - 1].length + 1
+// -104 <= triangle[i][j] <= 104
+ 
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& t) {
+        const int n=t.size();
+        vector<int> dp(n, t[0][0]);
+        for(int i=1; i<n; i++){
+            dp[i]=dp[i-1]+t[i][i];
+            for(int j=i-1; j>=1; j--){
+                dp[j]=t[i][j]+min(dp[j], dp[j-1]);
+            }
+            dp[0]+=t[i][0];
+        }
+        return *min_element(dp.begin(), dp.end());
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<vector<int>> triangle = {{2},{3,4},{6,5,7},{4,1,8,3}};
+    int result = sol.minimumTotal(triangle);
+    // Expected output: 11
+    return 0;
+}
