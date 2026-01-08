051584ca4cd6b4e8b2cb39707e9f83eac431399e
vivekpawar3010 <vivekpawar932564@gmail.com>
2026-01-08 19:53:36 +0530

    This is the date 8 jan simple day

---
+++ JAVA_Practice/JANUARY2026/JAN_8_topInterviewLeetCode.java
@@ -0,0 +1,89 @@
+// 13. Roman to Integer
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+// Symbol       Value
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+// For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+// Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+// I can be placed before V (5) and X (10) to make 4 and 9. 
+// X can be placed before L (50) and C (100) to make 40 and 90. 
+// C can be placed before D (500) and M (1000) to make 400 and 900.
+// Given a roman numeral, convert it to an integer.
+
+ 
+
+// Example 1:
+
+// Input: s = "III"
+// Output: 3
+// Explanation: III = 3.
+// Example 2:
+
+// Input: s = "LVIII"
+// Output: 58
+// Explanation: L = 50, V= 5, III = 3.
+// Example 3:
+
+// Input: s = "MCMXCIV"
+// Output: 1994
+// Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 15
+// s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+// It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+
+import java.util.*;
+
+class Solution {
+    public int romanToInt(String s) {
+        Map<Character, Integer> roman_values = new HashMap<>();
+        roman_values.put('I', 1);
+        roman_values.put('V', 5);
+        roman_values.put('X', 10);
+        roman_values.put('L', 50);
+        roman_values.put('C', 100);
+        roman_values.put('D', 500);
+        roman_values.put('M', 1000);
+
+        int total = 0;
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            int curr = roman_values.get(s.charAt(i));
+
+            if (i < n - 1 && curr < roman_values.get(s.charAt(i + 1))) {
+                total -= curr;
+            } else {
+                total += curr;
+            }
+        }
+
+        return total;
+    }
+}
+
+public class JAN_8_topInterviewLeetCode {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "MCMXCIV";
+        int result = solution.romanToInt(s);
+        System.out.println("The integer value of the Roman numeral " + s + " is: " + result);
+    }
+}
