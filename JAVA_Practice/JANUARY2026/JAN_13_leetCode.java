da9d157db63687f6d29836baa67462f90344186a
vivekpawar3010 <vivekpawar932564@gmail.com>
2026-01-13 20:33:38 +0530

    this is the date 13 jan 2025 be consistent

---
+++ JAVA_Practice/JANUARY2026/JAN_13_leetCode.java
@@ -0,0 +1,59 @@
+// 961. N-Repeated Element in Size 2N Array
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// You are given an integer array nums with the following properties:
+
+// nums.length == 2 * n.
+// nums contains n + 1 unique values, n of which occur exactly once in the array.
+// Exactly one element of nums is repeated n times.
+// Return the element that is repeated n times.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,3]
+// Output: 3
+// Example 2:
+
+// Input: nums = [2,1,2,5,3,2]
+// Output: 2
+// Example 3:
+
+// Input: nums = [5,1,5,2,5,3,5,4]
+// Output: 5
+ 
+
+// Constraints:
+
+// 2 <= n <= 5000
+// nums.length == 2 * n
+// 0 <= nums[i] <= 104
+// nums contains n + 1
+//  unique elements and one of them is repeated exactly n times.
+
+import java.util.*;
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        int n = nums.length / 2;
+        Set<Integer> seen = new HashSet<>();
+
+        for (int num : nums) {
+            if (!seen.add(num)) {
+                return num; // duplicate found
+            }
+        }
+        return 0;
+    }
+}
+public class JAN_13_leetCode {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums = {2,1,2,5,3,2};
+        int result = sol.repeatedNTimes(nums);
+        System.out.println(result); // Expected output: 2
+    }
+}
