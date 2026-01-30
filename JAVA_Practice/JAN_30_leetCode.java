e7cef3e3131b649db9f54887777df0eb2bf39aa8
vivekpawar3010 <vivekpawar932564@gmail.com>
2026-01-30 20:30:18 +0530

    this is the date 30 jan udpate new spirit new day

---
+++ JAVA_Practice/JAN_30_leetCode.java
@@ -0,0 +1,63 @@
+// 3701. Compute Alternating Sum
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You are given an integer array nums.
+
+// The alternating sum of nums is the value obtained by adding elements at even indices and subtracting elements at odd indices. That is, nums[0] - nums[1] + nums[2] - nums[3]...
+
+// Return an integer denoting the alternating sum of nums.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,3,5,7]
+
+// Output: -4
+
+// Explanation:
+
+// Elements at even indices are nums[0] = 1 and nums[2] = 5 because 0 and 2 are even numbers.
+// Elements at odd indices are nums[1] = 3 and nums[3] = 7 because 1 and 3 are odd numbers.
+// The alternating sum is nums[0] - nums[1] + nums[2] - nums[3] = 1 - 3 + 5 - 7 = -4.
+// Example 2:
+
+// Input: nums = [100]
+
+// Output: 100
+
+// Explanation:
+
+// The only element at even indices is nums[0] = 100 because 0 is an even number.
+// There are no elements on odd indices.
+// The alternating sum is nums[0] = 100.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 100
+// 1 <= nums[i] <= 100
+
+import java.util.*;
+class Solution {
+    public int alternatingSum(int[] nums) {
+        int result = 0;
+        for(int i = 0; i < nums.length; i += 2){
+            result += nums[i];
+        }
+        for(int i = 1; i < nums.length; i += 2){
+            result -= nums[i];
+        }
+        return result;
+    }
+}
+public class JAN_30_leetCode {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums = {1,3,5,7};
+        System.out.println(sol.alternatingSum(nums)); // Output: -4
+    }
+}
