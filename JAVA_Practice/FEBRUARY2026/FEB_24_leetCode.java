c549cb9d4230e0fd0bf06118393ab64cee94fb11
vivekpawar3010 <vivekpawar932564@gmail.com>
2026-02-24 08:46:32 +0530

    this is the date 24 feb be happy new gair change

---
+++ JAVA_Practice/FEBRUARY2026/FEB_24_leetCode.java
@@ -0,0 +1,68 @@
+// 1678. Goal Parser Interpretation
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You own a Goal Parser that can interpret a string command. The command consists of an alphabet of "G", "()" and/or "(al)" in some order. The Goal Parser will interpret "G" as the string "G", "()" as the string "o", and "(al)" as the string "al". The interpreted strings are then concatenated in the original order.
+
+// Given the string command, return the Goal Parser's interpretation of command.
+
+// Example 1:
+
+// Input: command = "G()(al)"
+// Output: "Goal"
+// Explanation: The Goal Parser interprets the command as follows:
+// G -> G
+// () -> o
+// (al) -> al
+// The final concatenated result is "Goal".
+// Example 2:
+
+// Input: command = "G()()()()(al)"
+// Output: "Gooooal"
+// Example 3:
+
+// Input: command = "(al)G(al)()()G"
+// Output: "alGalooG"
+
+// Constraints:
+
+// 1 <= command.length <= 100
+// command consists of "G", "()", and/or "(al)" in some order.
+
+import java.util.*;
+
+class Solution {
+    public String interpret(String command) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < command.length(); i++) {
+
+            if (command.charAt(i) == 'G') {
+                sb.append("G");
+
+            } else if (command.charAt(i) == '(') {
+
+                if (command.charAt(i + 1) == ')') {
+                    sb.append("o");
+                    i++; // skip ')'
+                } else { // (al)
+                    sb.append("al");
+                    i += 3; // skip 'a','l',')'
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+}
+
+public class FEB_24_leetCode {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String command = "G()(al)";
+        System.out.println(sol.interpret(command)); // Output: "Goal"
+    }
+}
