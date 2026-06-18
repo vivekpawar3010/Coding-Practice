2dae354ac332030acdc5987eb345b9fea1113f6e
Vivek Pawar <160049777+vivekpawar3010@users.noreply.github.com>
2026-06-18 17:08:18 +0530

    [852] [Time Beats: 100.00%] [Memory Beats: 93.54%] - LeetPush

--- JAVA_Practice/MAY2026/Peak-Index-in-a-Mountain-Array.java
+++ JAVA_Practice/MAY2026/Peak-Index-in-a-Mountain-Array.java
@@ -1,18 +1,17 @@
 1class Solution {
-2public:
-3    int peakIndexInMountainArray(vector<int>& arr) {
-4        int i = 1, j = arr.size() - 2;
-5    
-6        while(i <= j){
-7            int mid = i + (j - i) / 2;
-8            if(arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1]) {
-9                return mid;
-10            }else if(arr[mid - 1] < arr[mid] && arr[mid] < arr[mid + 1]){
-11                i = mid + 1;
-12            }else {
-13                j = mid - 1;
-14            }
-15        }
-16        return -1;
-17    }
-18};
+2    public int peakIndexInMountainArray(int[] arr) {
+3        int i = 1, j = arr.length - 2;
+4    
+5        while(i <= j){
+6            int mid = i + (j - i) / 2;
+7            if(arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1]) {
+8                return mid;
+9            }else if(arr[mid - 1] < arr[mid] && arr[mid] < arr[mid + 1]){
+10                i = mid + 1;
+11            }else {
+12                j = mid - 1;
+13            }
+14        }
+15        return -1;
+16    }
+17}
